a94223a840d762bc743bf976bff477f0870e533f
chewygumxx <cgumxx@pm.me>
2026-09-15 13:31:57 +1000

    fix(systemd/ssh): pass-cli ssh-agent service

--- env/base.env.zsh
+++ env/base.env.zsh
@@ -24,5 +24,3 @@ export BROWSER="firefox"
 export TERMCMD="wezterm"
 export EDITOR="nvim"
 export VISUAL="$EDITOR"
-
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
---
+++ env/ssh.env.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+# vim:set expandtab shiftwidth=4 filetype=zsh:
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+#
+# ~chewygumxx/dotfiles.git
+# ::: :/home/dot_config/zsh/env/ssh.env.zsh
+#
+#
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent-protonpass.socket"
