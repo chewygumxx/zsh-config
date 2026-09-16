040670b90fdaa2789161343d8accafb511cc5a2a
chewygumxx <cgumxx@pm.me>
2026-09-16 22:29:55 +1000

    tweak(zsh): Extract CLAUDE_CONFIG_DIR to zshenv

---
+++ env/claude.env.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+# vim:set expandtab shiftwidth=4 filetype=zsh:
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+#
+# ~chewygumxx/dotfiles.git
+# ::: :/home/dot_config/zsh/env/claude.env.zsh
+#
+#
+
+# Herdr also requires this
+: "${CLAUDE_CONFIG_DIR:=${XDG_CONFIG_HOME:-$HOME/.config}/claude}"
+export CLAUDE_CONFIG_DIR
